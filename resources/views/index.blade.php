0fc3431e0e3393b79a82833520818fb21535e4b5
ns <530822987@qq.com>
2019-03-02 12:31:29 +0800

    add head

---
+++ resources/views/index.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.main')
+
+@section('css')
+    {{Html::style('css/swiper/swiper.min.css')}}
+@endsection
+
+@section('title')
+    {{env('APP_NAME', '浙江财经大学|校友网')}}
+@endsection
+
+@section('body')
+    <!-- new line -->
+    {{--@include('partials._slider')--}}
+    {{--@include('blocks._news')--}}
+    {{--@include('blocks._sidebar')--}}
+    {{--<div class="clear" style="height: 40px"></div>--}}
+    {{--<!-- new line -->--}}
+    {{--@include('blocks._jobs')--}}
+    {{--@include('blocks._recommends')--}}
+    {{--@include('blocks._recruits')--}}
+    {{--<!-- new line -->--}}
+    {{--<div class="clear" style="height: 40px"></div>--}}
+    {{--@include('blocks._piclink')--}}
+    {{--@include('blocks._search_contact')--}}
+    {{--<div class="clear" style="height: 40px"></div>--}}
+    {{--@include('blocks._friendlink')--}}
+@endsection
+
+@section('script')
+    {{Html::script('js/swiper/swiper.min.js')}}
+    {{Html::script('js/embed_index.js')}}
+    <script type="text/javascript">
+        var mySwiper = new Swiper('.swiper-container', {
+            direction: 'horizontal',
+            loop: true,
+
+            pagination:{
+                el:'.swiper-pagination',
+            },
+
+            navigation:{
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        })
+
+
+    </script>
+
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+    {{Html::style('css/bootstrap.min.css')}}
+    @yield('css')
+    {{Html::style('css/normalize.css')}}
+    {{Html::style('css/style.css')}}
+</head>
+<body>
+@include('partials._head')
+{{--@include('partials._nav',['parent_menu' => $parent_menu])--}}
+{{--<div class="container">--}}
+    {{--@yield('body')--}}
+{{--</div>--}}
+{{--@include('partials._foot')--}}
+</body>
+
+{{Html::script('js/jquery-1.11.1.min.js')}}
+	{{Html::script('js/main.js')}}
+	<!--[if lt IE 9]>
+　 <script src="https://cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://cdn.bootcss.com/respond.js/1.4.2/respond.js"></script>
+<![endif]-->
+@yield('script')
+</html>
