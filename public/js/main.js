ef9e915232e4c1ce4459eeabbd0ec22ea7b87003
ns <530822987@qq.com>
2019-03-02 13:47:42 +0800

    add js

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+jquery-1.11.1.min.js$(function(){
+	var _navIndex = $(".nav>ul>li.current").index();
+	$(".nav>ul>li").hover(function(){
+		$(this).addClass("current").siblings().removeClass("current");
+		$(this).find("ul").stop().slideDown(300);
+	},function(){
+		$(this).removeClass("current");
+		$(".nav>ul>li").eq(_navIndex).addClass("current");
+		$(this).find("ul").stop().slideUp(300);
+	})
+})
