13e7bef31f6bb3dac9b481f2207ed56671322206
ns <530822987@qq.com>
2019-03-02 13:06:21 +0800

    add styles

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -1,6 +1,6 @@
 <div class="header_w">
     <div class="header">
-        <h1 class="header_logo"><a href="{{route('index')}}" title="{{env('APP_NAME')}}"><img src="{{asset('images/logo_main_xyw.jpg')}}" width="456" height="77" alt="{{env('APP_NAME')}}"></a></h1>
+        <h1 class="header_logo"><a href="{{route('index')}}" title="{{env('APP_NAME')}}"><img src="{{asset('images/logo_main_xyw.png')}}" width="456" height="77" alt="{{env('APP_NAME')}}"></a></h1>
         <div class="header_contact">
             <a href="{{route('index')}}" title="联系我们" class="current">联系我们</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#" title="企业入口">企业入口</a>
         </div>
