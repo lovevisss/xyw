0dcc916edb546003827a2cddfebc2440c23dba3b
ns <530822987@qq.com>
2019-03-02 16:19:24 +0800

    add slider scss

---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,18 @@
+<div class="footer_w">
+    <div class="footer clearfix">
+        <a href="http://xyw.zufedfc.edu.cn" class="footerLogo"><img src="http://xyw.zufedfc.edu.cn/images/logo_footer.png" width="280" height="62" alt=""></a>
+        <div class="footerInfo">
+            <h4 class="footerInfo_copy">版权所有 © 浙江财经大学东方学院学工部</h4>
+            <div class="footerInfo_con">
+                <p>地址：浙江省海宁市长安镇仰山路2号</p>
+                <p>电话：(86)573-87571010 邮编：314408 </p>
+                <p><a href="http://webscan.360.cn/index/checkwebsite/url/xyw.zufedfc.edu.cn">360网站安全监测</a></p>
+                <p>技术支持：<a href="http://it.zufedfc.edu.cn/" target="_blank" title="" style="color:#fff;">网络中心</a></p>
+            </div>
+        </div>
+        <div class="footerEwm">
+            {{--<img src="http://job.zufedfc.edu.cn/images/xg_weixin.jpg" width="104" height="104" alt="">--}}
+            {{--<div class="footerEwm_name">请关注公众号</div>--}}
+        </div>
+    </div>
+</div>
