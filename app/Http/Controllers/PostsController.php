bb033eb9bce54431bd6aaaf8981f36339dd6c612
ns <530822987@qq.com>
2019-03-04 04:13:21 +0800

    add pages

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -12,10 +12,9 @@ class PostsController extends Controller
     {
         $page = Post::where('id', '=', $id)->first();
         $title = $page->category->name;
-        echo $title;
-//        $menu = MenuItem::where('title', $title)->first();
-//        $menus = MenuItem::where('parent_id', $menu->parent_id)->get();
-//        $parent_menu = MenuItem::where('id', $menu->parent_id)->first();
-//        return view('pages.page', compact('page', 'title', 'menu', 'menus', 'parent_menu'));
+        $menu = MenuItem::where('title', $title)->first();
+        $menus = MenuItem::where('parent_id', $menu->parent_id)->get();
+        $parent_menu = MenuItem::where('id', $menu->parent_id)->first();
+        return view('pages.page', compact('page', 'title', 'menu', 'menus', 'parent_menu'));
     }
 }
