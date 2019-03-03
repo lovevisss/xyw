fc9b8b2299f3cb13e2782ac83e668218c7d1ff87
ns <530822987@qq.com>
2019-03-03 16:12:04 +0800

    add donate composer

--- resources/views/blocks/_donate.blade.php
+++ resources/views/blocks/_donate.blade.php
@@ -1,30 +1,32 @@
-@extends('layouts.partlist')
+{{--@extends('layouts.partlist')--}}
 
-@section('outerclass')
-    col-sm-3  small_list
-@endsection
+{{--@section('outerclass')--}}
+    {{--col-sm-3  small_list--}}
+{{--@endsection--}}
 
-@section('title')
-    {{$donate_posts[0]->category->name}}
-@stop
+{{--@section('title')--}}
+    {{--{{$donate_posts[0]->category->name}}--}}
+{{--@stop--}}
 
-@section('link')
-    {{route('category', ['id' => 1])}}
-@endsection
+{{--@section('link')--}}
+    {{--{{route('category', ['id' => 1])}}--}}
+{{--@endsection--}}
 
-@section('list')
-    @foreach($donate_posts as $new)
-        <li>
-            <a href="post/{{$new->id}}" title="{{$new->title}}">
-                <i class="glyphicon glyphicon-triangle-right"></i>
-                <strong>{{App\Helper\StringHelper::substrtitle($new->title,10)}}
-                    @if($new->created_at->diffInHours(\Carbon\Carbon::now()) < 72)
-                        <span class="new_info">new</span>
-                    @endif
-                </strong>
-                <span class="float-right">{{date_format($new->created_at,"m-d")}}</span>
-            </a>
-        </li>
-        <div class="clearfix">	</div>
-    @endforeach
-@stop
+{{--@section('list')--}}
+    {{--@foreach($donate_posts as $new)--}}
+        {{--<li>--}}
+            {{--<a href="post/{{$new->id}}" title="{{$new->title}}">--}}
+                {{--<i class="glyphicon glyphicon-triangle-right"></i>--}}
+                {{--<strong>{{App\Helper\StringHelper::substrtitle($new->title,10)}}--}}
+                    {{--@if($new->created_at->diffInHours(\Carbon\Carbon::now()) < 72)--}}
+                        {{--<span class="new_info">new</span>--}}
+                    {{--@endif--}}
+                {{--</strong>--}}
+                {{--<span class="float-right">{{date_format($new->created_at,"m-d")}}</span>--}}
+            {{--</a>--}}
+        {{--</li>--}}
+        {{--<div class="clearfix">	</div>--}}
+    {{--@endforeach--}}
+{{--@stop--}}
+
+donate
