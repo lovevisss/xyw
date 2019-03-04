b0c43a9054cb0c9eff8aa34561881442f85ed69f
ns <530822987@qq.com>
2019-03-04 10:02:40 +0800

    adjust css add cate 4

--- resources/views/blocks/_sidebar.blade.php
+++ resources/views/blocks/_sidebar.blade.php
@@ -1,8 +1,8 @@
 <div class="col-sm-3">
     <iframe src="//player.bilibili.com/player.html?aid=45075596&cid=78938184&page=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" width="230px"> </iframe>
-    <a href=""><img src="{{asset('1395801790.jpg')}}" alt=""></a>
-    <a href=""><img src="{{asset('1395801790.jpg')}}" alt=""></a>
-    <a href=""><img src="{{asset('1395801790.jpg')}}" alt=""></a>
+    <a href=""><img src="{{asset('images/1395801790.jpg')}}" alt=""></a>
+    <a href=""><img src="{{asset('images/1395801790.jpg')}}" alt=""></a>
+    <a href=""><img src="{{asset('images/1395801790.jpg')}}" alt=""></a>
 
 
 
