4dc469adfc0336234d58ce7ce92240d19566cafd
ns <530822987@qq.com>
2019-03-04 10:19:21 +0800

    adjust css add cate 5

--- resources/views/blocks/_sidebar.blade.php
+++ resources/views/blocks/_sidebar.blade.php
@@ -4,7 +4,7 @@
             学校视频
         </strong>
         <span class="in_titleLine"></span>
-        <a href="{{route('category',5)}}" class="in_titleMore">更多<i class="glyphicon glyphicon-chevron-right"></i></a>
+        <a href="{{route('category',5)}}" class="in_titleMore" style="padding-right: 35px; ">更多<i class="glyphicon glyphicon-chevron-right"></i></a>
     </div>
     <iframe src="//player.bilibili.com/player.html?aid=45075596&cid=78938184&page=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" width="230px"> </iframe>
     <a href=""><img src="{{asset('images/1395801790.jpg')}}" alt=""></a>
